fed916b2454a3d3629e0bae8aebd210bfd706f65
YaroslavGamayunov <yaroslav.gamayunov@gmail.com>
2021-04-18 21:48:20 +0300

    Added Yandex AppMetrica

--- app/src/main/java/com/yaroslavgamayunov/stockviewer/StockViewerApplication.kt
+++ app/src/main/java/com/yaroslavgamayunov/stockviewer/StockViewerApplication.kt
@@ -2,6 +2,8 @@ package com.yaroslavgamayunov.stockviewer
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 
 class StockViewerApplication : Application() {
     override fun onCreate() {
@@ -9,5 +11,21 @@ class StockViewerApplication : Application() {
 
         //TODO: Add support of dark theme
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setupAppMetrica()
+    }
+
+    private fun setupAppMetrica() {
+
+        // Creating an extended library configuration.
+        val config: YandexMetricaConfig =
+            YandexMetricaConfig.newConfigBuilder(BuildConfig.YANDEX_METRICA_API_TOKEN)
+                .withLogs()
+                .build()
+
+        // Initializing the AppMetrica SDK.
+        YandexMetrica.activate(applicationContext, config)
+
+        // Automatic tracking of user activity.
+        YandexMetrica.enableActivityAutoTracking(this)
     }
 }
